4257fce8ccb8c9e05e39e55b4b579f55014c30f2
Cristian Stoica <cristian.stoica@nxp.com>
2016-11-01 13:28:40 +0200

    add -async option to run_crypto_tests.sh
    Signed-off-by: Cristian Stoica <cristian.stoica@nxp.com>

--- tests/run_crypto_tests.sh
+++ tests/run_crypto_tests.sh
@@ -4,6 +4,31 @@
 BUF_SIZE="8192 16384 65536 131072 262144"
 THREAD_CNT="1 8 12"
 ALG_NAMES="aes-128-cbc aes-256-xts sha1 sha256 crc32c"
+TIME=10
+
+############################
+
+function usage
+{
+cat << EOF
+Usage: `basename $0` [OPTIONS]
+
+  -a              run async version of the benchmark (default sync)
+  -h              show this help
+
+Run in sequence benchmarks for several crypto algorithms:
+$ALG_NAMES
+EOF
+}
+
+while getopts ah option
+do
+    case "$option" in
+	a) aflag="-a";;
+	*) usage $0; exit 1;;
+    esac
+done
+
 
 #restool dpseci create --num-queues=8 --priorities=1,2,3,4,5,6,7,8
 #restool dprc assign dprc.1 --object=dpseci.0 --plugged=1
@@ -16,7 +41,11 @@ do
 	do
 		for bsize in ${BUF_SIZE}
 		do
-			speed_multi.sh -t 10 -n $bsize -m ${multi} ${alg_name} |
+			speed_multi.sh -t ${TIME}\
+				-n ${bsize}\
+				-m ${multi}\
+				${aflag}\
+				${alg_name} |
 			tail -n 1
 		done
 	done
